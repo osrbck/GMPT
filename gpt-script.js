0e47952f103181fb93e624e020b51454e42dd137
Osman S <osrbck@gmail.com>
2023-08-28 21:34:37 +0300

    get the chat responses

--- gpt-script.js
+++ gpt-script.js
@@ -2,6 +2,20 @@ console.log('GMPT init');
 
 chrome.runtime.onMessage.addListener(
     (emailContent, sender, sendResponse) => {
-        console.log(emailContent);
+        const textArea = document.querySelector('textarea');
+        textArea.value = 'Respond to the most recent email in a professional tone: \n';
+        const button = textArea.nextElementSibling;
+        button.click();
+
+        const callback = function(mutationList, observer){
+            for(const mutation of mutationList){
+                if(mutation.attributeName ==='disabled'){
+                    if(button.disabled ===false) console.log('GPT response finished');
+                }
+            }
+        };
+
+        const observer = new MutationObserver(callback);
+        observer.observe(button, {attributes: true});
     }
 )
