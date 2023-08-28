b534d1d1041bb9079ee1552ddbfca31fe05dc0d9
Osman S <osrbck@gmail.com>
2023-08-28 22:12:32 +0300

    installation complete

--- gmail-script.js
+++ gmail-script.js
@@ -1,23 +1,22 @@
-window.onload = function()
-{
+window.onload = function () {
     window.onhashchange = () => {
         if (window.location.hash.startsWith('#inbox/')) {
             const spans = document.querySelectorAll('span');
-
-            for(const span of spans){
-                    if (span.innerText === 'Reply') {
-                        //do something on click
-                        span.addEventListener('click' ,function(){
-                            const email = document.querySelector('.adn.ads');
-                            (async function() {
-                                const gptResponse = await chrome.runtime.sendMessage(email.textContent);
-                                const gmailTextbox = document.querySelector('[role=textbox]');
-                                gmailTextbox.innerText=gptResponse;
-                            })();
-                        });
-                    }
+    
+            for (const span of spans) {
+              if (span.innerText === 'Reply') {
+                span.addEventListener('click', function() {
+                    const email = document.querySelector('.adn.ads');
+                    (async function() {
+						console.log(email.textContent);
+                        const gptResponse = await chrome.runtime.sendMessage(email.textContent);
+						console.log(gptResponse);
+                        const gmailTextbox = document.querySelector('[role=textbox]');
+                        gmailTextbox.innerText = gptResponse;
+                    })();
+                });
+              }
             }
         }
     };
-    return true;
 }
--- gpt-script.js
+++ gpt-script.js
@@ -1,27 +1,30 @@
-console.log('GMPT init');
+console.log('hi im the gpt script');
 
 chrome.runtime.onMessage.addListener(
-    (emailContent, sender, sendResponse) => {
+    function(emailContent, sender, sendResponse) {
         const textArea = document.querySelector('textarea');
-        textArea.value = 'Respond to the most recent email in a professional tone: \n' + emailContent;
-        const button = textArea.nextElementSibling;
-        button.click();
-
-        const callback = function(mutationList, observer){
-            for(const mutation of mutationList){
-                if(mutation.attributeName ==='disabled'){
-                    if(button.disabled ===false) {
-                        const responses = document.querySelector('#__next > div > div > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.relative.flex.h-full.max-w-full.flex-1.overflow-hidden > div > main > div > div.flex-1.overflow-hidden > div > div > div').childNodes;
-                        const lastResponse = responses[responses.length-2];
-                        const lastResponseText = lastResponse.innerText;
-                        sendResponse(lastResponseText);
-                    }
-                }
-            }
+		
+		let n = 1;
+        const callback = function(mutationList, observer) {
+			if (n === 3) {
+				const responses = document.querySelector('#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.relative.flex.h-full.max-w-full.flex-1.overflow-hidden > div > main > div > div.flex-1.overflow-hidden > div > div > div').childNodes;
+				const lastResponse = responses[responses.length - 2];
+				const lastResponseText = lastResponse.innerText.slice(9);
+			    sendResponse(lastResponseText);
+			}
+			n++;
         };
-
+        const button = textArea.nextElementSibling;
         const observer = new MutationObserver(callback);
         observer.observe(button, {attributes: true});
+		
+		const inputEvent = new Event('input', {
+			'bubbles': true,
+			'cancelable': true
+		});
+        textArea.value = 'Respond to the most recent email in a professional tone and sign off with my name (Herman) at the end:\n' + emailContent;
+		textArea.dispatchEvent(inputEvent);
+		button.click()
         return true;
     }
 )
